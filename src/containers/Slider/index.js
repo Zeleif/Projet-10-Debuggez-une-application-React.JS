0c8832c90ceb77a2161785e94bd4e9d3bfab6e60
Zeleif <alyomeguja@gmail.com>
2023-11-09 16:13:55 +0100

    debug slider

--- src/containers/Slider/index.js
+++ src/containers/Slider/index.js
@@ -1,18 +1,17 @@
 import { useEffect, useState } from "react";
 import { useData } from "../../contexts/DataContext";
 import { getMonth } from "../../helpers/Date";
-
 import "./style.scss";
 
 const Slider = () => {
   const { data } = useData();
   const [index, setIndex] = useState(0);
   const byDateDesc = data?.focus.sort((evtA, evtB) =>
-    new Date(evtA.date) < new Date(evtB.date) ? -1 : 1
+  new Date(evtB.date) - new Date(evtA.date)
   );
   const nextCard = () => {
     setTimeout(
-      () => setIndex(index < byDateDesc.length ? index + 1 : 0),
+      () => setIndex(index < byDateDesc.length - 1 ? index + 1 : 0),
       5000
     );
   };
@@ -21,20 +20,20 @@ const Slider = () => {
   });
   return (
     <div className="SlideCardList">
-      {byDateDesc?.map((event, idx) => (
-        <>
+      {byDateDesc?.map((events, idx) => (
+           <>
           <div
-            key={event.title}
+            key={events.title}
             className={`SlideCard SlideCard--${
               index === idx ? "display" : "hide"
             }`}
           >
-            <img src={event.cover} alt="forum" />
+            <img src={events.cover} alt="forum" />
             <div className="SlideCard__descriptionContainer">
               <div className="SlideCard__description">
-                <h3>{event.title}</h3>
-                <p>{event.description}</p>
-                <div>{getMonth(new Date(event.date))}</div>
+                <h3>{events.title}</h3>
+                <p>{events.description}</p>
+                <div>{getMonth(new Date(events.date))}</div>
               </div>
             </div>
           </div>
@@ -42,18 +41,19 @@ const Slider = () => {
             <div className="SlideCard__pagination">
               {byDateDesc.map((_, radioIdx) => (
                 <input
-                  key={`${event.id}`}
+                key={`${byDateDesc[radioIdx].id}`}
                   type="radio"
                   name="radio-button"
-                  checked={idx === radioIdx}
-                />
-              ))}
-            </div>
-          </div>
-        </>
-      ))}
-    </div>
-  );
-};
-
+                  checked={index === radioIdx}
+                  />
+                  ))}
+                </div>
+              </div>
+            </>
+          ))}
+        </div>
+      );
+    };
+    
+ 
 export default Slider;
