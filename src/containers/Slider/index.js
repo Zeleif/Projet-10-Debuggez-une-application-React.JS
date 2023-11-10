11e3e14b3d1e15a5baffc36d24a70a9662b15c8a
Zeleif <alyomeguja@gmail.com>
2023-11-10 20:22:07 +0100

    push final

--- src/containers/Slider/index.js
+++ src/containers/Slider/index.js
@@ -47,7 +47,7 @@ const Slider = () => {
               type="radio"
               name="radio-button"
               checked={index === radioIdx}
-              onChange={() => { }}
+              onChange={() => setIndex(radioIdx)}
             />
           ))}
         </div>
